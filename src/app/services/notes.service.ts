52b11484bcb9337622f6c6db144e785458e4e034
Phillip MWaniki Nzuli <mwanikiphillip@gmail.com>
2018-10-04 18:50:08 +0300

    added @ionic/storage

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Storage } from "@ionic/storage";
+
+import { Note } from "../interfaces/notes";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
 
-  constructor() { }
+  public notes: Note[] = [];
+  public loaded: boolean = false;
+  constructor(private storage: Storage) { }
+
+  load(): Promise<boolean> {
+    return new Promise((resolve) => {
+
+      this.storage.get('notes').then((notes) => {
+        if (notes != null) {
+          this.notes = notes;
+        }
+
+        this.loaded = true;
+        resolve(true);
+      });
+    });
+  }
+
+  save(): void {
+    this.storage.set('notes', this.notes);
+  }
+
+  getNote(id): Note {
+    return this.notes.find(note => note.id === id);
+  }
+
+  createNote(title): void {
+    let id = Math.max(...this.notes.map(note => parseInt(note.id)), 0) + 1;
+
+    this.notes.push({
+      id: id.toString(),
+      title: title,
+      content: ''
+    });
+  }
+
+  deleteNote(note): void {
+    let index = this.notes.indexOf(note);
+
+    if (index > -1){
+      this.notes.splice(index, 1);
+      this.save();
+    }
+  }
 }
