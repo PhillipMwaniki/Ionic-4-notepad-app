720decd38581de0212cdabd1fafb374cd9c8ea82
Phillip MWaniki Nzuli <mwanikiphillip@gmail.com>
2018-10-04 19:25:20 +0300

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'notes', pathMatch: 'full' },
   { path: 'notes', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'Detail', loadChildren: './detail/detail.module#DetailPageModule' },
+  { path: 'notes/:id', loadChildren: './detail/detail.module#DetailPageModule' },
 ];
 
 @NgModule({
